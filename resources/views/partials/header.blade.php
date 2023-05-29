755f3079c3853d6b3f31902ea325be9afc6d3a9f
ariannadrovandi <ariannadrovandi02@gmail.com>
2023-05-29 16:40:33 +0200

    added header & footer

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,14 @@
+<header class="bg-white">
+    <div class="container d-flex justify-content-between align-items-center">
+        <div>
+            <img class="img-fluid" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Logo">
+        </div>
+        <nav>
+            <ul class="d-flex flex-wrap align-items-center list-unstyled">
+                @foreach (config('partials.navLinks') as $link)
+                <li class="pe-4"><a href="#">{{$link}}</a></li>
+                @endforeach
+            </ul>
+        </nav>
+    </div>
+</header>
